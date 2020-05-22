7ee9d359a30a89af2ff81146cb6e6c09b3e9fc54
teddydamian <teddy.damian@gmail.com>
2020-05-21 20:58:17 -0700

    summary comments

--- DogsIRL/DogsIRL/RegisterPage.xaml.cs
+++ DogsIRL/DogsIRL/RegisterPage.xaml.cs
@@ -20,7 +20,10 @@ namespace DogsIRL
             _apiAccountService = new ApiAccountService();
         }
 
-
+        /// <summary>
+        /// This method is wired to register button in the front. It will grab user input and create an account. It will then send a request to backend API.
+        /// Backend API will response back with a token. We also set global username as current username.
+        /// </summary>
         async void OnButtonClicked(System.Object sender, System.EventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(username.Text) && !string.IsNullOrWhiteSpace(email.Text))
@@ -43,7 +46,7 @@ namespace DogsIRL
                     await DisplayAlert("Registration failed", "Oh no! We weren't able to register. Make sure your username, email, and password are valid, or try again later.", "Return");
                 }
                 App.Username = model.Username;
-               string token = await _apiAccountService.RequestJwtTokenFromApi();
+                string token = await _apiAccountService.RequestJwtTokenFromApi();
                 App.Token = token;
                 await Navigation.PushAsync(new ProfileView());
             }
