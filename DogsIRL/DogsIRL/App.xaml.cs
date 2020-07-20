6481022f1a3f2b715d7e28b3b88ab6cd731bd4d2
Teddy Damian <Teddy Damian@gmail.com>
2020-07-20 11:28:02 -0700

    Jauth token added

--- DogsIRL/DogsIRL/App.xaml.cs
+++ DogsIRL/DogsIRL/App.xaml.cs
@@ -9,9 +9,9 @@ namespace DogsIRL
     public partial class App : Application
     {
         public static string Token;
-        public static string ApiUrl = "https://dogs-irl-api.azurewebsites.net/api";
-        //public static string ApiUrl = "https://localhost:44357/api";
-        //public static string ApiUrl = "https://10.0.2.2:5001/api";
+        //public static string ApiUrl = "https://dogs-irl-api.azurewebsites.net/api";
+        //public static string ApiUrl = "https://localhost:44317/api";
+        public static string ApiUrl = "https://10.0.2.2:44317/api";
         public static string Username;
         public static PetCard CurrentDog;
 
