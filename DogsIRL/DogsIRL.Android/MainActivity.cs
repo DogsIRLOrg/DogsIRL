89125b1492e48be39ff7fe6be3024aac922c0267
andrewbc.dev@outlook.com <andrewbc.dev@outlook.com>
2020-09-06 23:17:38 -0700

    Added icons, privacy policy, and project files for android release

--- DogsIRL/DogsIRL.Android/MainActivity.cs
+++ DogsIRL/DogsIRL.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Xamd.ImageCarousel.Forms.Plugin.Droid;
 
 namespace DogsIRL.Droid
 {
-    [Activity(Label = "DogsIRL", Icon = "@drawable/gus", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "DogsIRL", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
